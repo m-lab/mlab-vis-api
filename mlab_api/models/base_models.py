2fe4e5baf4e0cb1e259673f4ef97717292df9da0
Jim Vallandingham <vlandham@gmail.com>
2016-08-25 14:45:39 -0700

    include codes in output.

--- mlab_api/models/base_models.py
+++ mlab_api/models/base_models.py
@@ -9,9 +9,12 @@ from mlab_api.rest_api import api
 # Base Models
 # ---
 base_meta_fields = api.model('Base Meta', {
-    'client_continent': fields.String(description="Parent continent of Location."),
-    'client_country': fields.String(description="Parent country of Location."),
-    'client_region': fields.String(description="Parent region of Location."),
+    'client_continent': fields.String(description="Continent of Location."),
+    'client_continent_code': fields.String(description="Continent code of Location."),
+    'client_country': fields.String(description="Country of Location."),
+    'client_country_code': fields.String(description="Country code of Location."),
+    'client_region': fields.String(description="Region of Location."),
+    'client_region_code': fields.String(description="Region code of Location."),
     'client_city': fields.String(description="Name of city, if location is a city.")
 })
 
