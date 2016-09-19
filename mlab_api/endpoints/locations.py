af20f08d8d020da6e4b6fafdb0aa64a1d1a667bb
Jim Vallandingham <vlandham@gmail.com>
2016-09-19 14:12:30 -0700

    make stats names consistent.

--- mlab_api/endpoints/locations.py
+++ mlab_api/endpoints/locations.py
@@ -29,7 +29,7 @@ class LocationInfo(Resource):
     Location Info
     '''
     # @api.marshal_with(location_search_model)
-    @statsd.timer('location.info.api')
+    @statsd.timer('locations.info.api')
     def get(self, location_id):
         """
         Location Info
@@ -49,7 +49,7 @@ class LocationChildren(Resource):
     '''
     @api.expect(type_arguments)
     @api.marshal_with(location_children_model)
-    @statsd.timer('location.children.api')
+    @statsd.timer('locations.children.api')
     def get(self, location_id):
         """
         Location Search
@@ -68,7 +68,7 @@ class LocationSearch(Resource):
     Location Search Resource
     '''
     @api.marshal_with(location_search_model)
-    @statsd.timer('location.search.api')
+    @statsd.timer('locations.search.api')
     def get(self, location_query):
         """
         Location Search
@@ -88,7 +88,7 @@ class LocationTimeMetric(Resource):
 
     @api.expect(date_arguments)
     @api.marshal_with(location_metric_model)
-    @statsd.timer('location.metrics.api')
+    @statsd.timer('locations.metrics.api')
     def get(self, location_id, time_aggregation):
         """
         Get Location Metrics Over Time
@@ -112,7 +112,7 @@ class LocationTimeClientIspMetric(Resource):
     '''
 
     @api.expect(date_arguments)
-    @statsd.timer('location.clientisps_metrics.api')
+    @statsd.timer('locations.clientisps_metrics.api')
     def get(self, location_id, time_aggregation, client_isp_id):
         """
         Get Location Metrics for ISP Over Time
@@ -138,7 +138,7 @@ class LocationClientIspStats(Resource):
     '''
 
     @api.expect(include_data_arguments)
-    @statsd.timer('location.clientisps_list.api')
+    @statsd.timer('locations.clientisps_list.api')
     def get(self, location_id):
         """
         Get Location Metrics for ISP Over Time
@@ -161,7 +161,7 @@ class LocationClientIspInfo(Resource):
     '''
 
     @api.marshal_with(location_client_isp_info_model)
-    @statsd.timer('location.clientisps_info.api')
+    @statsd.timer('locations.clientisps_info.api')
     def get(self, location_id, client_isp_id):
         """
         Get ISP info for a specific location / isp combo
