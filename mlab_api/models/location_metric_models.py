ac43bf0e4391b2d4d952aa7d3291a774cea9daa7
Peter Beshai <peter.beshai@gmail.com>
2016-10-12 12:35:01 -0400

    Add models and to CSV for location+servers and loc+cli+serv

--- mlab_api/models/location_metric_models.py
+++ mlab_api/models/location_metric_models.py
@@ -8,6 +8,7 @@ from mlab_api.format_utils import meta_results_to_csv
 
 from mlab_api.models.base_models import location_base_meta_fields, metric_data_fields
 from mlab_api.models.asn_models import client_metric_meta_fields
+from mlab_api.models.asn_models import server_metric_meta_fields
 
 metric_meta_fields = location_base_meta_fields.extend('Location Metric Meta', {
 })
@@ -22,7 +23,7 @@ def location_metric_to_csv(data):
     return meta_results_to_csv(data, metric_meta_fields, metric_data_fields)
 
 
-
+# Location + Client
 location_client_meta_fields = metric_meta_fields.extend('Location+Client Metric Meta',
     client_metric_meta_fields)
 
@@ -33,3 +34,30 @@ location_client_metric_model = api.model('Location+Client Metric Model', {
 
 def location_client_metric_to_csv(data):
     return meta_results_to_csv(data, location_client_meta_fields, metric_data_fields)
+
+
+
+# Location + Server
+location_server_meta_fields = metric_meta_fields.extend('Location+Server Metric Meta',
+    server_metric_meta_fields)
+
+location_server_metric_model = api.model('Location+Server Metric Model', {
+    'meta': fields.Nested(location_server_meta_fields, required=True),
+    'results': fields.List(fields.Nested(metric_data_fields), required=True)
+})
+
+def location_server_metric_to_csv(data):
+    return meta_results_to_csv(data, location_server_meta_fields, metric_data_fields)
+
+
+# Location + Client + Server
+location_client_server_meta_fields = location_client_meta_fields.extend('Location+Client+Server Metric Meta',
+    server_metric_meta_fields)
+
+location_client_server_metric_model = api.model('Location+Client+Server Metric Model', {
+    'meta': fields.Nested(location_client_server_meta_fields, required=True),
+    'results': fields.List(fields.Nested(metric_data_fields), required=True)
+})
+
+def location_client_server_metric_to_csv(data):
+    return meta_results_to_csv(data, location_client_server_meta_fields, metric_data_fields)
