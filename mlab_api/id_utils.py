d1d5e015fe344f6571c76b21e7c633e1f6132d1c
Peter Beshai <peter.beshai@gmail.com>
2016-10-12 13:28:13 -0400

    Add location+servers list to model and CSV

--- mlab_api/id_utils.py
+++ mlab_api/id_utils.py
@@ -5,3 +5,11 @@ def location_client_id(d):
     '''
     return '%s_%s' % (d['location_key'] if 'location_key' in d else d['client_location_key'],
                       d['client_asn_number'])
+
+
+def location_server_id(d):
+    '''
+    Create composite ID for locations + servers
+    '''
+    return '%s_%s' % (d['location_key'] if 'location_key' in d else d['client_location_key'],
+                      d['server_asn_number'])
--- mlab_api/models/location_info_models.py
+++ mlab_api/models/location_info_models.py
@@ -7,8 +7,9 @@ from mlab_api.rest_api import api
 
 
 from mlab_api.models.base_models import location_base_meta_fields
+from mlab_api.models.asn_models import client_asn_search_meta_fields, server_asn_search_meta_fields
 from mlab_api.format_utils import meta_data_to_csv, meta_data_in_row_to_csv, meta_in_row_to_csv
-from mlab_api.id_utils import location_client_id
+from mlab_api.id_utils import location_client_id, location_server_id
 
 location_info_meta_fields = location_base_meta_fields.extend('Location Info Meta', {
     'id': fields.String(description="Location Id"),
@@ -63,14 +64,13 @@ def location_children_to_csv(data):
     return meta_data_in_row_to_csv(data, location_info_meta_fields, location_info_data_fields)
 
 
-
-location_client_asn_meta_fields = location_info_meta_fields.extend('Location Client ASN Meta', {
-    'client_asn_name': fields.String(description="Name of ASN."),
-    'client_asn_number': fields.String(description="ASN number."),
-    'last_year_test_count': fields.Integer(description="Test counts in last year"),
-    'location_key': fields.String(description="Location Id"),
-    'id': fields.String(description="Location+Clients Id", attribute=location_client_id),
-})
+# ------------------------------------------------------
+# Location + Client Info
+# ------------------------------------------------------
+location_client_asn_meta_fields = location_info_meta_fields.extend('Location Client ASN Meta (no ID)',
+    client_asn_search_meta_fields).extend('Location Client ASN Meta', {
+        'id': fields.String(description="Location+Clients Id", attribute=location_client_id),
+    })
 
 api.models[location_client_asn_meta_fields.name] = location_client_asn_meta_fields
 
@@ -83,6 +83,9 @@ def location_client_isp_info_to_csv(data):
     return meta_data_to_csv(data, location_client_asn_meta_fields, location_client_isp_data_fields)
 
 
+# ------------------------------------------------------
+# Location + Client List
+# ------------------------------------------------------
 location_clients_list_model = api.model('Location Client List Model', {
     "results": fields.List(fields.Nested({
         'meta': fields.Nested(location_client_asn_meta_fields, required=True)
@@ -91,3 +94,21 @@ location_clients_list_model = api.model('Location Client List Model', {
 
 def location_clients_list_to_csv(data):
     return meta_in_row_to_csv(data, location_client_asn_meta_fields)
+
+
+# ------------------------------------------------------
+# Location + Server List
+# ------------------------------------------------------
+location_server_asn_meta_fields = location_info_meta_fields.extend('Location Server ASN Meta (no ID)',
+    server_asn_search_meta_fields).extend('Location Server ASN Meta', {
+        'id': fields.String(description="Location+Servers Id", attribute=location_server_id),
+    })
+
+location_servers_list_model = api.model('Location Server List Model', {
+    "results": fields.List(fields.Nested({
+        'meta': fields.Nested(location_server_asn_meta_fields, required=True)
+    }))
+})
+
+def location_servers_list_to_csv(data):
+    return meta_in_row_to_csv(data, location_server_asn_meta_fields)
