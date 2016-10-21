d4a8ea56fdc84a5af38bfe32f07e58124a54b31f
Jim Vallandingham <vlandham@gmail.com>
2016-10-21 15:53:32 -0700

    start on tracking

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ GOOGLE_PROJECT_ID = 'mlab-oti'
 BIGTABLE_INSTANCE = 'mlab-ndt-agg'
 BIGTABLE_POOL_SIZE = 40
 STATSD_SERVER='104.155.133.245'
+GA_TRACKING_ID='UA-86118171-1'
 
 # BIGTABLE_CONFIG_DIR = '../pipeline/dataflow/data/bigtable'
 BIGTABLE_CONFIG_DIR = 'bigtable_configs'
--- mlab_api/stats.py
+++ mlab_api/stats.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import requests
 from statsd import StatsClient
 
 from mlab_api.app import app
@@ -10,3 +11,28 @@ if not host:
 
 #Instance of statsd client to import
 statsd = StatsClient(host=host, prefix="mlab")
+
+tracking_id = app.config['GA_TRACKING_ID']
+
+
+def track_event(category, action, label=None, value=0):
+    data = {
+        'v': '1',  # API Version.
+        'tid': tracking_id,  # Tracking ID / Property ID.
+        # Anonymous Client Identifier. Ideally, this should be a UUID that
+        # is associated with particular user, device, or browser instance.
+        'cid': '555',
+        't': 'event',  # Event hit type.
+        'ec': category,  # Event category.
+        'ea': action,  # Event action.
+        'el': label,  # Event label.
+        'ev': value,  # Event value, must be an integer
+    }
+
+    response = requests.post(
+        'http://www.google-analytics.com/collect', data=data)
+
+    # If the request fails, this will raise a RequestException. Depending
+    # on your application's needs, this may be a non-error and can be caught
+    # by the caller.
+    response.raise_for_status()
