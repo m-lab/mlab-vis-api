a6d20c64e0ef9188ab06fce4f224f5455e3a53e0
Jim Vallandingham <vlandham@gmail.com>
2016-10-24 11:30:39 -0700

    sort by last year test count

--- mlab_api/sort_utils.py
+++ mlab_api/sort_utils.py
@@ -9,9 +9,9 @@ def sort_by_count(row):
     Given a row, return its test_count or last_year_test_count,
     if present
     '''
-    if 'test_count' in row['meta']:
-        return row['meta']['test_count']
-    elif 'last_year_test_count' in row['meta']:
+    if 'last_year_test_count' in row['meta']:
         return row['meta']['last_year_test_count']
+    elif 'test_count' in row['meta']:
+        return row['meta']['test_count']
     else:
         return 'a'
